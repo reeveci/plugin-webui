95c7fa11867667b02eb87e2636c55bf9b37f90e5
Aaron <4709780+2manyvcos@users.noreply.github.com>
2024-06-09 19:51:23 +0000

    Update client deps

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 3000 },
+  server: { port: 8080, host: true },
   plugins: [react()],
   build: { outDir: "dist" },
 });
