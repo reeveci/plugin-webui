97a09b8415ad5b28f81483221e125007597ff923
Aaron <4709780+2manyvcos@users.noreply.github.com>
2025-02-01 06:15:41 +0000

    Vite: allow any host

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 8080, host: true },
+  server: { port: 8080, host: true, allowedHosts: true },
   plugins: [react()],
   resolve: {
     alias: [
